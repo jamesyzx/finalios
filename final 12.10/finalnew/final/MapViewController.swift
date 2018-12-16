4e3b3880c33df32b67b8c9c7db3769a14b3ad888
Yu zixuan <yu.zix@husky.neu.edu>
2018-12-15 22:40:45 -0500

    final 12.15

--- final 12.10/finalnew/final/MapViewController.swift
+++ final 12.10/finalnew/final/MapViewController.swift
@@ -54,33 +54,31 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, HomeModelP
         camera = GMSCameraPosition.camera(withLatitude: 42.340375, longitude: -71.089100, zoom: zoom)
         viewMap.camera = camera
 
-//        camera = GMSCameraPosition.camera(withLatitude: 42.340375, longitude: -71.089100, zoom: 15)
-//        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-//        view = mapView
+
 
         
-//        // Creates a marker in the center of the map.
-//        let marker = GMSMarker()
-//        marker.position = CLLocationCoordinate2D(latitude: 42.340375, longitude: -71.089100)
-//        marker.title = "My Location"
-//        marker.snippet = ""
-//        marker.map = viewMap
-//
-//        let marker2 = GMSMarker()
-//        marker2.position = CLLocationCoordinate2D(latitude: 42.343492, longitude: -71.087051)
-//        marker2.title = "Player2"
-//        marker2.snippet = "rank"
-//        marker2.map = viewMap
-//        marker2.icon = GMSMarker.markerImage(with: .green)
-//
-//        let marker3 = GMSMarker()
-//        marker3.position = CLLocationCoordinate2D(latitude: 42.338425, longitude: -71.087813)
-//        marker3.title = "Player3"
-//        marker3.snippet = "Rank"
-//        marker3.map = viewMap
-//        marker3.icon = GMSMarker.markerImage(with: .green)
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: 42.340375, longitude: -71.089100)
+        marker.title = "My Location"
+        marker.snippet = ""
+        marker.map = viewMap
+
+        let marker2 = GMSMarker()
+        marker2.position = CLLocationCoordinate2D(latitude: 42.343492, longitude: -71.087051)
+        marker2.title = "Player2"
+        marker2.snippet = "rank"
+        marker2.map = viewMap
+        marker2.icon = GMSMarker.markerImage(with: .green)
+
+        let marker3 = GMSMarker()
+        marker3.position = CLLocationCoordinate2D(latitude: 42.338425, longitude: -71.087813)
+        marker3.title = "Player3"
+        marker3.snippet = "Rank"
+        marker3.map = viewMap
+        marker3.icon = GMSMarker.markerImage(with: .green)
         
-        // Do any additional setup after loading the view.
+       
     }
     
     
